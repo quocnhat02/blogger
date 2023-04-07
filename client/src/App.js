2ba4a238e747856720943b09a7974f1f24bbd9e5
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-04-07 23:09:08 +0700

    css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+  return <div className='bg-red-500 flex justify-center'>Home</div>;
+};
+
+export default Home;
